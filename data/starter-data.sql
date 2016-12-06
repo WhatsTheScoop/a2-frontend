426c4615fa68346d1a7cdf11948448170b86d660
Alex Young <alexyoung612@gmail.com>
2016-12-06 09:56:32 -0800

    fixed database primary keys.

--- data/starter-data.sql
+++ data/starter-data.sql
@@ -19,7 +19,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `whatsthescoop`
 --
-
 -- --------------------------------------------------------
 
 --
@@ -28,13 +27,13 @@ SET time_zone = "+00:00";
 DROP TABLE IF EXISTS `ingredients`;
 
 CREATE TABLE `ingredients` (
-  `id` int(64) NOT NULL,
+  `id` int(64) PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `name` varchar(64) NOT NULL,
   `price` integer(64) NOT NULL,
   `type` varchar(64) NOT NULL,
   `perBox` int(64) NOT NULL,
   `onHand` int(64) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=16;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=0;
 
 --
 -- Dumping data for table `ingredients`
@@ -61,28 +60,28 @@ INSERT INTO `ingredients` (`name`, `price`, `type`, `perBox`, `onHand`) VALUES
 DROP TABLE IF EXISTS `recipes`;
 
 CREATE TABLE `recipes` (
-  `id` int(16) NOT NULL,
+  `id` int(16) PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `code` varchar(32) NOT NULL,
   `description` varchar(256) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=0;
 
 --
 -- Dumping data for table `recipes`
 --
 
-INSERT INTO `recipes` (`id`, `code`, `description`) VALUES
-('0', 'Child Cone','One small scoop of vanilla, perfect for a child!'),
-('1', 'Large Vanilla', 'Two scoops of vanilla goodness'),
-('2', 'Small Chocolate', 'One scoop of chocolate, just enough to make you want more!'),
-('3', 'Large Chocolate', 'Two scoops of chocolate, we won''t tell if you don''t!'),
-('4', 'Chocolate Extreme', 'Two scoops of chocolate, plus toppings, everyone loves chocolate!'),
-('5', 'Napolean', 'One scoop of vanilla, chocolate, and strawberry!'),
-('6', 'Feeling Nutty', 'One scoop of maple, with lots of walnuts for a topping!'),
-('7', 'Minty Fresh', 'One scoop of mint ice cream.'),
-('8', 'Oh Canada', 'Two scoops of maple ice cream.'),
-('9', 'Attempted Rainbow', 'One scoop of strawberry, orange, vanilla, and chocolate!'),
-('10', 'I can''t Choose', 'One scoop of each ice cream with all of the toppings!'),
-('11', 'Forgetting Something', 'Hmm is something missing?');
+INSERT INTO `recipes` (`code`, `description`) VALUES
+('Child Cone','One small scoop of vanilla, perfect for a child!'),
+('Large Vanilla', 'Two scoops of vanilla goodness'),
+('Small Chocolate', 'One scoop of chocolate, just enough to make you want more!'),
+('Large Chocolate', 'Two scoops of chocolate, we won''t tell if you don''t!'),
+('Chocolate Extreme', 'Two scoops of chocolate, plus toppings, everyone loves chocolate!'),
+('Napolean', 'One scoop of vanilla, chocolate, and strawberry!'),
+('Feeling Nutty', 'One scoop of maple, with lots of walnuts for a topping!'),
+('Minty Fresh', 'One scoop of mint ice cream.'),
+('Oh Canada', 'Two scoops of maple ice cream.'),
+('Attempted Rainbow', 'One scoop of strawberry, orange, vanilla, and chocolate!'),
+('I can''t Choose', 'One scoop of each ice cream with all of the toppings!'),
+('Forgetting Something', 'Hmm is something missing?');
 
 -- --------------------------------------------------------
 
@@ -93,59 +92,59 @@ INSERT INTO `recipes` (`id`, `code`, `description`) VALUES
 DROP TABLE IF EXISTS `recipeingredients`;
 
 CREATE TABLE `recipeingredients` (
-  `id` int(16) NOT NULL,
+  `id` int(16) PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `recipeid` int(16) NOT NULL,
   `ingredientid` int(16) NOT NULL,
   `quantity` int(16) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=0;
 
 --
 -- Dumping data for table `ingredients`
 --
 
 INSERT INTO `recipeingredients` (`recipeid`, `ingredientid`, `quantity`) VALUES 
-('0', '1', '1'),
-('0', '3', '1'),
-('1', '0', '1'),
-('1', '3', '2'),
+('1', '2', '1'),
+('1', '4', '1'),
 ('2', '1', '1'),
-('2', '5', '2'),
-('3', '0', '1'),
-('3', '5', '2'),
-('4', '2', '1'),
-('4', '5', '2'),
-('4', '11', '1'),
-('5', '0', '1'),
-('5', '5', '1'),
+('2', '4', '2'),
+('3', '2', '1'),
+('3', '6', '2'),
+('4', '1', '1'),
+('4', '6', '2'),
 ('5', '3', '1'),
-('5', '4', '1'),
-('6', '0', '1'),
-('6', '7', '1'),
-('6', '10', '1'),
+('5', '6', '2'),
+('5', '12', '1'),
+('6', '1', '1'),
+('6', '6', '1'),
+('6', '4', '1'),
+('6', '5', '1'),
 ('7', '1', '1'),
-('7', '6', '1'),
-('8', '0', '1'),
-('8', '7', '2'),
-('9', '2', '1'),
-('9', '3', '1'),
-('9', '4', '3'),
-('9', '8', '1'),
-('9', '5', '1'),
-('9', '9', '1'),
-('10', '2', '1'),
+('7', '8', '1'),
+('7', '11', '1'),
+('8', '2', '1'),
+('8', '7', '1'),
+('9', '1', '1'),
+('9', '8', '2'),
 ('10', '3', '1'),
-('10', '4', '3'),
-('10', '8', '1'),
-('10', '5', '1'),
-('10', '6', '1'),
-('10', '7', '1'),
+('10', '4', '1'),
+('10', '5', '3'),
 ('10', '9', '1'),
+('10', '6', '1'),
 ('10', '10', '1'),
-('10', '11', '1'),
-('11', '0', '1'),
+('11', '3', '1'),
+('11', '4', '1'),
+('11', '5', '3'),
 ('11', '9', '1'),
+('11', '6', '1'),
+('11', '7', '1'),
+('11', '8', '1'),
 ('11', '10', '1'),
-('11', '11', '1');
+('11', '11', '1'),
+('11', '12', '1'),
+('12', '1', '1'),
+('12', '10', '1'),
+('12', '11', '1'),
+('12', '12', '1');
 
 
 -- --------------------------------------------------------

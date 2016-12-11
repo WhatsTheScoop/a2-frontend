698543ba29c421b810fc8c0f277528fecd3fae46
Francis Quintal <francisquintal.fq@gmail.com>
2016-12-10 23:50:48 -0800

    changed homepage to show overview of our menu and have a link at the bottom to our sales page (broken right now b/c sales isnt ready).

--- application/views/header.php
+++ application/views/header.php
@@ -52,11 +52,11 @@
 <!-- Header Carousel -->
 <header id="myCarousel" class="carousel slide">
     <!-- Wrapper for slides -->
-    <div class="carousel-inner">
-        <div class="item active">
+    <div class="carousel-inner" style="">
+        <div class="item active" >
             <div class="fill" style="background-image:url('http://del.h-cdn.co/assets/15/26/980x490/landscape-1435086195-ice-cream-cones-delish-9.JPG');"></div>
             <div class="carousel-caption">
-                <h2>Welcome</h2>
+                <h1>Assignment #2 From Comp 4711</h1>
             </div>
         </div>
     </div>
